11ad4a8526ac292fb6515cd687b3bab94553a6f7
NPException <npexception42@gmail.com>
2020-02-02 13:44:21 +0100

    finish genealogy namespace

--- Clojure port/src/java_after_eight_clj/genealogy.clj
+++ Clojure port/src/java_after_eight_clj/genealogy.clj
@@ -36,33 +36,30 @@
   [article-1 article-2 relation-type score]
   (when-not (<= 0 score 100)
     (throw (ex-info "Score should be in interval [0; 100]" {:score score})))
-  {:article-1     (util/assert-not-nil article-1)
-   :article-2     (util/assert-not-nil article-2)
-   :relation-type (util/assert-not-nil relation-type)
-   :score         score})
+  {:articles [(util/assert-not-nil article-1)
+              (util/assert-not-nil article-2)]
+   :type     (util/assert-not-nil relation-type)
+   :score    score})
 
 
 (defn ^:private create-relation
-  [article-1 article-2 score]
+  [articles score]
   (when-not (<= 0 score 100)
     (throw (ex-info "Score should be in interval [0; 100]" {:score score})))
-  {:article-1 article-1
-   :article-2 article-2
-   :score     score})
+  {:articles articles
+   :score    score})
 
 
 (defn ^:private create-unfinished-relation
-  [{:keys [article-1 article-2 score] :as typed-relation} ^double weight]
-  {:article-1   article-1
-   :article-2   article-2
+  [{:keys [articles score] :as typed-relation} ^double weight]
+  {:articles    articles
    :score-total (* score weight)
    :score-count 1})
 
 
 (defn ^:private fold-unfinished-relations
   [ur-1 ur-2]
-  (when (not= (select-keys ur-1 [:article-1 :article-2])
-              (select-keys ur-2 [:article-1 :article-2]))
+  (when (not= (:articles ur-1) (:articles ur-2))
     (ex-info "All typed relations must belong to the same article."
              {:unfinished-relation-1 ur-1
               :unfinished-relation-2 ur-2}))
@@ -76,12 +73,12 @@
   [typed-relations weights]
   (util/assert-not-empty
     typed-relations "Can't create relation from zero typed relations.")
-  (let [{:keys [article-1 article-2 score-total score-count]}
+  (let [{:keys [articles score-total score-count]}
         (->> typed-relations
              (map #(create-unfinished-relation % (weights (:type %))))
              (reduce fold-unfinished-relations))]
     (create-relation
-      article-1 article-2
+      articles
       (Math/round ^double (/ score-total score-count)))))
 
 
@@ -95,12 +92,18 @@
 
 (defn ^:private aggregate-typed-relations
   [typed-relations weights]
-  ;; TODO 1: implement Genealogy.aggregateTypedRelations here
-  ;; TODO 2: add tests
-  )
+  (->> typed-relations
+       (reduce
+         (fn [result {:keys [articles] :as relation}]
+           (update result articles conj relation))
+         {})
+       (mapv #(aggregate-relation (val %) weights))))
 
 
 (defn infer-relations
   [articles genealogists weights]
+  (util/assert-not-empty articles)
+  (util/assert-not-empty genealogists)
+  (util/assert-not-nil weights)
   (-> (infer-typed-relations articles genealogists)
       (aggregate-typed-relations weights)))
